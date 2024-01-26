20a3ff4d36b82e5b1f3bb8d3b13058b35f50479c
Dima Myagkov <dimon.mykish@gmail.com>
2024-01-26 22:43:02 +0300

    step #5

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import greetingsOfUser from '../src/cli.js';
+import firstGame from './brain-even.js';
 
 console.log('Welcome to the Brain Games!');
 
 greetingsOfUser();
+
+firstGame();
--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,11 @@
 
 import readlineSync from 'readline-sync';
 
+export var userName;
+
 export default function greetingsOfUser() {
 
-//var readlineSync = require('readline-sync');
-
-// Wait for user's response.
-var userName = readlineSync.question('May I have your name? ');
+userName = readlineSync.question('May I have your name? ');
 
 return console.log('Hello, ' + userName + '!');
 
